9c5e21e2b5fe3e8cb9c9ab223a3024b86b77520c
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-23 00:46:41 +0500

    add second example

--- exeption_handling.py
+++ exeption_handling.py
@@ -1,1 +1,11 @@
-print("hello world")
+while True:
+    try:
+        age = int(input("Enter your age "))
+        break
+    except ValueError:
+        print("invalid value.. Please try again")
+
+if age < 18:
+    print("you can not play this game ")
+else:
+    print("you can play ")
