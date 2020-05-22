80753e5456eb03a2b1fe30a83b8c67281184c109
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-23 01:10:26 +0500

    add final example

--- else_finally.py
+++ else_finally.py
@@ -1,10 +1,12 @@
-try:
-    number = int(input("Enter number "))
-except ValueError:
-    print("you did'nt Enter Integer ")
-except:
-    print("unknown Error ")
-else:
-    print(f"You enter {number}")
-finally:
-    print("its final  !!")
+while True:
+    try:
+        number = int(input("Enter number "))
+    except ValueError:
+        print("you did'nt Enter Integer ")
+    except:
+        print("unknown Error ")
+    else:
+        print(f"You enter {number}")
+        break
+    finally:
+        print("its final  !!")
